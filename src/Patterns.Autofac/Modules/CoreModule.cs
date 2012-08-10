e4ebec808ad9c88c05dbe417435a71057e6d64d3
John Batte <john.batte@icainformatics.com>
2012-08-10 09:30:18 -0500

    Patterns.Testing + Patterns.Autofac + specs
    Major Features:
    1. Patterns.Autofac.Sources.ResolveAnythingSource
    2. Patterns.Autofac.Modules.CoreModule
    3. Patterns.Testing.Runtime.TestDateTimeInfo

---
+++ src/Patterns.Autofac/Modules/CoreModule.cs
@@ -0,0 +1,43 @@
+﻿#region New BSD License
+
+// Copyright (c) 2012, John Batte
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+// 
+// Redistributions of source code must retain the above copyright notice, this list of conditions
+// and the following disclaimer.
+// 
+// Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+// and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+using Autofac;
+
+using Patterns.Runtime;
+
+namespace Patterns.Autofac.Modules
+{
+	/// <summary>
+	/// Provides packaged registration instructions for default implementations
+	/// of public contracts defined in the Patterns library.
+	/// </summary>
+	public class CoreModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<DefaultDateTimeInfo>().As<IDateTimeInfo>();
+		}
+	}
+}
---
+++ src/_specs.Patterns.Autofac/Features/CoreModule.feature
@@ -0,0 +1,11 @@
+﻿Feature: Core Module
+	As a developer, I want to be able to register a single module for Patterns
+	and get the default implementations of each public interface defined in
+	the core library.
+
+@refreshContainer
+Scenario: Runtime - IDateTimeInfo
+	Given I have registered the core module
+	And I have created the container
+	When I try to resolve the IDateTimeInfo interface
+	Then the resolved object should be an instance of DefaultDateTimeInfo
