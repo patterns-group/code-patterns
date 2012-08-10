b6b7eb69341d81657b952b3b7b6f67024f3355ce
John Batte <john.batte@icainformatics.com>
2012-08-09 19:32:49 -0500

    _specs.Common
    1. Extensions for SpecFlow's ScenarioContext object
    2. Text parsing for advanced scenario outline support

---
+++ src/_specs.Common/TextParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Patterns.Specifications.Common
+{
+	public class TextParser
+	{
+		private const string _simpleStringDictionaryParseErrorFormat = "Could not parse {0} as a simple string dictionary. Format must be: key1:value1;key2:value2";
+
+		public static IDictionary<string, string> ParseSimpleDictionaryString(string text)
+		{
+			try
+			{
+				return text.Split(';').Select(pair => pair.Split(':')).ToDictionary(item => item[0], item => item[1]);
+			}
+			catch
+			{
+				throw new Exception(string.Format(_simpleStringDictionaryParseErrorFormat, text));
+			}
+		}
+	}
+}
