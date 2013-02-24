3809784ff7dbda00113b729b98e97449602425dd
John Batte <john.batte@icainformatics.com>
2013-02-24 02:03:17 -0600

    issue #34: completed xml code docs for existing types

--- src/Patterns/Logging/LoggingInterceptor.cs
+++ src/Patterns/Logging/LoggingInterceptor.cs
@@ -1,25 +1,21 @@
-﻿#region New BSD License
+﻿#region FreeBSD
 
-// // Copyright (c) 2013, John Batte
-// // All rights reserved.
-// // 
-// // Redistribution and use in source and binary forms, with or without modification, are permitted
-// // provided that the following conditions are met:
-// // 
-// // Redistributions of source code must retain the above copyright notice, this list of conditions
-// // and the following disclaimer.
-// // 
-// // Redistributions in binary form must reproduce the above copyright notice, this list of conditions
-// // and the following disclaimer in the documentation and/or other materials provided with the distribution.
-// // 
-// // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-// // WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-// // PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-// // ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-// // TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// // HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// // POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2013, John Batte
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 
+//  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// 
+//  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #endregion
 
@@ -32,21 +28,37 @@ using Common.Logging;
 
 namespace Patterns.Logging
 {
+	/// <summary>
+	///    Provides an <see cref="IInterceptor" /> implementation that
+	///    uses <see cref="ILog" /> to log Trace, Debug, Info, and Error
+	///    events throughout the execution of intercepted invocations.
+	/// </summary>
 	public class LoggingInterceptor : IInterceptor
 	{
 		private const string _nullArgument = "[NULL]";
 		private const string _argumentListFormat = "({0})";
 		private const string _argumentListSeparator = ",";
 		private const string _stringDisplayFormat = @"""{0}""";
-		private readonly bool _trapInterceptedExceptions;
 		private readonly Func<Type, ILog> _logFactory;
+		private readonly bool _trapInterceptedExceptions;
 
+		/// <summary>
+		///    Initializes a new instance of the <see cref="LoggingInterceptor" /> class.
+		/// </summary>
+		/// <param name="trapInterceptedExceptions">
+		///    if set to <c>true</c>, intercepted exceptions are trapped and do not bubble up.
+		/// </param>
+		/// <param name="logFactory">The log factory.</param>
 		public LoggingInterceptor(bool trapInterceptedExceptions, Func<Type, ILog> logFactory)
 		{
 			_trapInterceptedExceptions = trapInterceptedExceptions;
 			_logFactory = logFactory;
 		}
 
+		/// <summary>
+		///    Intercepts the specified invocation.
+		/// </summary>
+		/// <param name="invocation">The invocation.</param>
 		public void Intercept(IInvocation invocation)
 		{
 			ILog log = _logFactory(invocation.TargetType);
