b34c5e112918abc3f74e47f4de5376a0a57a7219
Khoa Nguyen <khoa.nguyen@icainformatics.com>
2015-03-31 16:05:55 -0500

    WIP - Adding stream extensions and some tests

---
+++ src/_specs/Features/Stream/ToArray.feature
@@ -0,0 +1,10 @@
+﻿Feature: Convert a stream to a byte array
+	As a developer, I want to easily convert a stream to a byte array
+	the same way MemoryStream.ToArray() works even when I don't know
+	what kind of stream I have
+
+@stream
+Scenario: Conversion preserve data
+	Given I have a memory stream of "This is data"
+	When I call ToArray on the memory stream
+	Then the result byte array should be "This is data"
---
+++ src/_specs/Steps/Stream/StreamExtensionsSteps.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Text;
+using FluentAssertions;
+using Patterns.Stream;
+using TechTalk.SpecFlow;
+
+namespace Patterns.Specifications.Steps.Stream
+{
+	[Binding]
+	public class StreamExtensionsSteps
+	{
+
+		private MemoryStream _memoryStream;
+
+		[Given(@"I have a memory stream of ""(.*)""")]
+		public void GivenIHaveAMemoryStreamOf(string p0)
+		{
+			_memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(p0));
+		}
+
+		[Given(@"I set the position to be at the end")]
+		public void GivenISetThePositionToBeAtTheEnd()
+		{
+			_memoryStream.Seek(0, SeekOrigin.End);
+		}
+
+		[When(@"I call reset position")]
+		public void WhenIPressCallResetPosition()
+		{
+			_memoryStream.ResetPosition();
+		}
+
+		[Then(@"the position should be at the beginning")]
+		public void ThenThePositionShouldBeAtTheBeginning()
+		{
+			_memoryStream.Position.Should().Be(0);
+		}
+		
+
+
+		//private System.IO.Stream _stream;
+		//private byte[] _conversionResult;
+
+		//[Given(@"I have a memory stream of ""(.*)""")]
+		//public void GivenIHaveAMemoryStreamOf(string p0)
+		//{
+		//	_stream = new MemoryStream(Encoding.UTF8.GetBytes(p0));
+		//}
+		
+
+
+		//[Given(@"I set the position to be at the end")]
+		//public void GivenISetThePositionToBeAtTheEnd()
+		//{
+		//	_stream.Seek(0, SeekOrigin.End);
+		//}
+		
+		//[When(@"I call reset position")]
+		//public void WhenIPressCallResetPosition()
+		//{
+		//	_stream.ResetPosition();
+		//}
+
+		//[Then(@"the position should be at the beginning")]
+		//public void ThenThePositionShouldBeAtTheBeginning()
+		//{
+		//	_stream.Position.Should().Be(0);
+		//}
+
+		//[When(@"I call ToArray")]
+		//public void WhenICallToArray()
+		//{
+		//	_conversionResult = _stream.ToArray();
+		//}
+
+
+		//[Then(@"the result byte array should be ""(.*)""")]
+		//public void ThenTheResultByteArrayShouldBe(string p0)
+		//{
+		//	ScenarioContext.Current.Pending();
+		//}
+	}
+}
